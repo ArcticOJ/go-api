54ffd58be97bd80f0e6325a1c873fa30768ac497
Nguyen Thanh Quang <admin@xwork.space>
2023-12-03 19:55:30 +0700

    :sparkles: allow specifying custom user-agent

--- client.go
+++ client.go
@@ -17,12 +17,12 @@ type Logger interface {
 	Fatal(format string, args ...interface{})
 }
 
-func NewClient(baseUrl string, logger Logger) (c *Client) {
+func NewClient(baseUrl string, userAgent string, logger Logger) (c *Client) {
 	if logger == nil {
 		logger = &NilLogger{}
 	}
 	c = &Client{
-		c: req.C().SetBaseURL(baseUrl).SetCommonHeader("User-Agent", "iceberg/0.1.0-alpha (https://github.com/ArcticOJ/iceberg)"),
+		c: req.C().SetBaseURL(baseUrl).SetCommonHeader("User-Agent", userAgent),
 		l: logger,
 	}
 	return c
